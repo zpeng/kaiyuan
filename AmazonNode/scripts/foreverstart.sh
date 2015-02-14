8233658c3aae026777eb95a47feb167d0361086e
jiansen <jiansen@gmail.com>
2015-02-14 18:25:06 +0000

    update

--- AmazonNode/scripts/foreverstart.sh
+++ AmazonNode/scripts/foreverstart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# node cannot start on port 80, so we set the iptables
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+
 cd ~/kaiyuan/AmazonNode/NodeServer/app/ServerWorker
 
 # Invoke the Forever module (to START our Node.js server).
 forever start -al forever.log -ao out.log -ae err.log index.js 3000
-
-# node cannot start on port 80, so we set the iptables
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
