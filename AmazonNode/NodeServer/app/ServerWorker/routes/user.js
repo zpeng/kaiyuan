dc8af1eb706f2e972b6d4ed3713ebdc40bce9422
jiansen <jiansen@gmail.com>
2015-02-22 16:13:30 +0000

    map empty path to index.html

--- AmazonNode/NodeServer/app/ServerWorker/routes/user.js
+++ AmazonNode/NodeServer/app/ServerWorker/routes/user.js
@@ -1,23 +1,22 @@
-var express = require('express');
-var router = express.Router();
-
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	// TODO: how to distinguish client and admin?
-	res.redirect('/');
-}
+var express = require('express'),
+    router = express.Router(),
+    path = require('path'),
+    isAuthenticated = function (req, res, next) {
+		// if user is authenticated in the session, call the next() to call the next request handler 
+		// Passport adds this method to request object. A middleware is allowed to add properties to
+		// request and response objects
+		if (req.isAuthenticated())
+			return next();
+		// if the user is not authenticated then redirect him to the login page
+		// TODO: how to distinguish client and admin?
+		res.redirect('/');
+	};
 
 module.exports = function(passport){
 
 	/* GET login page. */
 	router.get('/', function(req, res) {
-    	// Display the Login page with any flash message, if any
-		res.render('index', { message: req.flash('message') });
+    	res.sendFile('index.html', { root: path.join(__dirname, '../') });
 	});
 
 	/* Handle Login POST */
